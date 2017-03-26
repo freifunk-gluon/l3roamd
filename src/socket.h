631b19448d4d69c8abecf9b03f059933953cdb91
Christof Schulze <christof.schulze@gmx.net>
2017-03-26 15:53:04 +0200

    add very basic status-socket to l3roamd
    The unix-socket will report a json structure that at the moment contains the amount of connected clients only.
    This introduces a "-s" command line switch to activate the socket

---
+++ src/socket.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stddef.h>
+#include <inttypes.h>
+
+
+typedef struct {
+	struct l3ctx *l3ctx;
+	int fd;
+	struct sockaddr_un socket;
+} socket_ctx;
+
+void socket_init(socket_ctx *ctx, char *path);
+void socket_handle_in(socket_ctx *ctx, size_t count);
